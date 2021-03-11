8a928308570d5720c8f7090e6802eb4d3b258b4c
Adekunte Tolulope <38886632+Adtrex@users.noreply.github.com>
2021-03-11 22:19:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 
-	  <style>
+	<!--  <style>
 .editor-stage .snow {
   height:50px;
   background: #fff;
@@ -63,7 +63,7 @@
   50% {background-position: 500px 500px, 100px 200px, -100px 150px;}
   100% {background-position: 500px 1000px, 200px 400px, -100px 300px;}
 }
-</style>
+</style>-->
 	
 </head>
 <body class="body">
